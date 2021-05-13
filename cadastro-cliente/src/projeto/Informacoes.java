b591e4c43ba9018e42d130acb0b4d5103054790c
Stefany Santos <stefanyjenifer1@hotmail.com>
2021-05-12 23:57:18 -0300

    cadastro cliente

---
+++ cadastro-cliente/src/projeto/Informacoes.java
@@ -0,0 +1,13 @@
+package projeto;
+
+public class Informacoes {
+	
+	   private String nome;
+	   
+	   private int senha;
+	   
+	   private String login;
+	   
+	   private String cargo;
+
+}
---
+++ cadastro-cliente/src/projeto/Usuarios.java
@@ -0,0 +1,46 @@
+package projeto;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Usuarios {
+	public static void main(String[] args) {
+	     
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("BEM VINDO AO CADASTRO DE USUÁRIOS");
+        
+        System.out.println("Digite os dados: "+"\nNome ");
+        
+        String nome = teclado.nextLine();
+        
+        System.out.println("Login desejado ");
+        
+        String login = teclado.nextLine();
+        
+        System.out.println("Cargo ");
+        
+        String cargo = teclado.nextLine();
+        
+        System.out.println("Senha de acesso");
+        
+        int senha = teclado.nextInt();
+        
+        
+        Informacoes usuario;
+        
+        usuario = new Informacoes();
+        
+     //   usuario.setNome(nome);
+     //   usuario.setLogin(login);
+     //   usuario.setCargo(cargo);
+        
+        
+        ArrayList<Informacoes> usuarios = new ArrayList<Informacoes>();
+        
+        usuarios.add(usuario);
+        
+      //  System.out.println(usuario.getNome()+"\n"+usuario.getLogin()+"\n"+usuario.getCargo());
+     }
+}
+
